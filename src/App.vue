1232005e2fd4e94554b354e00d453a38e666fe87
Jarrod Seccombe <jarrod@cynical.software>
2019-09-03 21:02:52 -0400

    finish app skeleton design

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,56 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">TEAM </span>
+        <span>TRON</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer
+      class="light-blue darken-2"
+      app
+      clipped
+      dark
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-volume-high</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Sounds</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import {Component, Vue} from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
-export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-});
+@Component({
+  components: {HelloWorld},
+})
+export default class App extends Vue {
+  public name: string = 'App';
+  public drawer = null;
+}
 </script>
---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-navigation-drawer class="deep-purple accent-4" app clipped dark>
+    <v-list>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-volume-high</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Sounds</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+@Component
+export default class NavDrawer extends Vue {}
+</script>
